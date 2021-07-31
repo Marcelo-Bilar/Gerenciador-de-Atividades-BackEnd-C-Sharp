feed0e1c23736b15a7807af03d6a3740861242fb
Marcelo Bilar Rodrigues <marcelo.bilar2@gmail.com>
2021-07-31 20:08:50 -0300

    Primeira parte do projeto api c# .net core

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Dtos/ErroRespostaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Dtos
+{
+    public class ErroRespostaDto
+    {
+        public int Status { get; set; }
+        public string Erro { get; set; }
+    }
+}
